4079d0c9e6f1dfb3aba8d0d629cc108837a9a770
samwhelp <samwhelp@users.noreply.github.com>
2024-05-09 19:10:58 +0800

    fix

--- helper/mak/help.mk
+++ helper/mak/help.mk
@@ -11,10 +11,18 @@ help:
 	@echo
 	@echo '	$$ make fetch-all'
 	@echo
+	@echo '	$$ make fetch-grub-theme-glass-remix'
+	@echo
+	@echo '	$$ make fetch-grub-theme-cover-remix'
+	@echo
 	@echo '	$$ make fetch-grub-theme-monterey-remix'
 	@echo
 	@echo '	$$ make fetch-grub-theme-obs-remix'
 	@echo
+	@echo
+	@echo '	$$ make apply-grub-theme-glass-remix'
+	@echo
+	@echo '	$$ make apply-grub-theme-cover-remix'
 	@echo
 	@echo '	$$ make apply-grub-theme-monterey-remix'
 	@echo
